6be1ac5981471dfb39545c5a5d026b56f2f5ff0d
Noono0 <khe9010@naver.com>
2022-02-14 05:38:24 +0000

    0214

---
+++ 프로그래머스/인프런/revers.py
@@ -0,0 +1,27 @@
+
+import sys
+import time
+import itertools
+
+
+start = time.time() 
+#sys.stdin=open("input.txt", "r")
+#import math
+#n = int(sys.stdin.readline())
+#d
+
+
+def solve1(self, s: List[str]) -> None:
+  return s[::-1]
+
+
+
+
+
+
+
+print(Solution().solve1('lovelo'))
+
+
+print("")
+print("time:",time.time() - start)
